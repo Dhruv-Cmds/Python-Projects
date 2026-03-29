66e6924ac1e1bf76cdf54d5decce0dc2684df14b
Dhruv-Cmds <pd1900565@gmail.com>
2026-03-29 21:00:13 +0530

    Added data store in json formate

--- practice/Student Management System/v2_oop_file/Main.py
+++ practice/Student Management System/v2_oop_file/Main.py
@@ -1,40 +1,74 @@
+import json
+
 class Student:
-    def __init__(self, name, rollno, filepath="student.txt"):
+
+    def __init__(self, name, rollno, filepath="practice/Student Management System/v2_oop_file/student.json"): # filepath="student.txt"
         self.name = name
         self.rollno = rollno
         self.filepath = filepath
         self.marks = []
         self.load()
 
-    def save(self):
-        with open(self.filepath, "w") as f:
-            marks_str = ",".join(map(str, self.marks))
-            f.write(f"{self.name} {self.rollno} {marks_str}\n")
+    # def save(self):
 
-    def load(self):
+    #     with open(self.filepath, "w") as f:
+    #         marks_str = ",".join(map(str, self.marks))
+    #         f.write(f"{self.name} {self.rollno} {marks_str}\n")
+
+    # def load(self):
+
+    #     try:
+    #         with open(self.filepath, "r") as f:
+    #             data = f.read().split()
+    #             if len(data) >= 3:
+    #                 self.name = data[0]
+    #                 self.rollno = int(data[1])
+    #                 self.marks = list(map(int, data[2].split(",")))
+
+    #     except FileNotFoundError:
+    #         self.save()
+
+    def save (self):
+
+        with open (self.filepath , "w") as f:
+            json.dump ({"name" : self.name, "roll_no" : self.rollno, "marks" : self.marks}, f, indent=4)
+
+    def load (self):
+
         try:
-            with open(self.filepath, "r") as f:
-                data = f.read().split()
-                if len(data) >= 3:
-                    self.name = data[0]
-                    self.rollno = int(data[1])
-                    self.marks = list(map(int, data[2].split(",")))
+             
+            with open (self.filepath , "r") as f:
+
+                data = json.load(f)
+
+                if data: 
+                    self.name = data.get("name" , self.name)
+                    self.rollno = data.get("roll_no" , self.rollno)
+                    self.marks = data.get("marks" , self.marks)
+                
+                else:
+                    self.save()
+        
         except FileNotFoundError:
             self.save()
 
     def add_marks(self, mark):
+
         self.marks.append(mark)
         self.save()
 
     def calculate_average(self):
+
         if not self.marks:
             return 0
         return sum(self.marks) / len(self.marks)
 
     def pass_fail(self):
+
         return "You passed the exam." if self.calculate_average() >= 40 else "You failed the exam."
 
     def info(self):
+
         print(f"Name: {self.name}")
         print(f"RollNo: {self.rollno}")
         print(f"Marks: {self.marks}")
